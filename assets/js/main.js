e2ea1d8fbb6f2c2fbab7956551cf4a5528501d50
soyaibur <ovisoyaiburrahman@gmail.com>
2020-05-09 23:26:20 +0600

    Owl-corousel funtion added in main.js

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+/* <script> 
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin: 15,
+    nav:true,
+    navText:["<i class='fa fa-long-arrow-left'>","<i class='fa fa-long-arrow-right'>"],
+    hover:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+    </script> */
